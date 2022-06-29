68a6d044087652ed9accf97f80827e7d1b9a0ef2
Jackfrost24a <100822818+Jackfrost24a@users.noreply.github.com>
2022-06-29 21:46:52 +0700

    BG-4 001

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import React from 'react';
-import LandingPage from './Pages/Landing';
-import NavbarComponent from './Components/Navbar';
+import React from "react";
+import LandingPage from "./Pages/Landing";
+import NavbarComponent from "./Components/Navbar";
+import Leftbar from "./Pages/Leftbar";
+
 function App() {
   return (
     <div>
-    <NavbarComponent />
+      <NavbarComponent />
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
+      <Leftbar />
     </div>
   );
 }
